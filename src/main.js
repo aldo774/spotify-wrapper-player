34e0e9321c06bfef368a591045d86f43220cde58
Aldo Soares <soares_aldo@hotmail.com>
2018-03-28 21:37:25 -0300

    [IMP] Inclusão de dependencia com a lib de spotify-wrapper e criação de testes para sua integração

---
+++ src/main.js
@@ -0,0 +1,1 @@
+console.log('Spotify Wrapper Player');
---
+++ src/spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'token',
+});
+
+export default spotify;
