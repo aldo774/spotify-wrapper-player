9c5cf569dc6264e3d486f9d6e327f318032da772
Aldo Soares <soares_aldo@hotmail.com>
2018-03-29 02:11:00 -0300

    [IMP] Implementação de evento que reproduz a faixa do album ao ser clicada

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import searchEnterTrigger from './SearchTrigger';
 import searchAlbumTrigger from './SelectAlbumTrigger';
+import playlistTrigger from './PlaylistTrigger';
 
 searchEnterTrigger();
 searchAlbumTrigger();
+playlistTrigger();
