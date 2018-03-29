95af379f3a0998e6bb725ed00cdef90bc15bb16c
Aldo Soares <soares_aldo@hotmail.com>
2018-03-29 01:50:38 -0300

    [IMP] Implementação de evento para exibição de album e faixas do album ao clicar em qualquer album da lista de albums

--- src/SearchTrigger.js
+++ src/SearchTrigger.js
@@ -6,10 +6,14 @@ const albumList = document.getElementById('album-list');
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
+function makeRequest(data) {
+  spotify.search.albums(searchInput.value)
+    .then(data => renderAlbums(data.albums.items, albumList))
+}
+
 export default function searchEnterTrigger() {
   searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    spotify.search.albums(searchInput.value)
-      .then(data => renderAlbums(data.albums.items, albumList))
+    makeRequest();
   });
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
-import spotify from './Spotify';
-import renderAlbumInfo from './AlbumInfo';
-import renderAlbumTracks from './AlbumTracks';
 import searchEnterTrigger from './SearchTrigger';
+import searchAlbumTrigger from './SelectAlbumTrigger';
 
 searchEnterTrigger();
-
-const album = spotify.album.getAlbum('0lw68yx3MhKflWFqCsGkIs');
-const albumInfo = document.getElementById('album-info');
-const albumTracks = document.getElementById('album-tracks');
-
-album
-  .then(data => renderAlbumInfo(data, albumInfo))
-  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
+searchAlbumTrigger();
